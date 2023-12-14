5ce49a6a54fd1c166b3926816a94dd66ba7a8b5e
sdrawkcabdaeruoynac <105550246+sdrawkcabdaeruoynac@users.noreply.github.com>
2023-12-14 17:40:35 -0500

    Update what_the_elf.py

--- what_the_elf.py
+++ what_the_elf.py
@@ -12,24 +12,17 @@ def main():
     item_dictionary.set_row_and_columns(len(lines), len(lines[0]) - 1)
     item_dictionary.generate(lines)
 
-    x = 0
     sum = 0
-    while x < len(item_dictionary.numbers) :
-        value = item_dictionary.numbers[x][0]
-        items = item_dictionary.numbers[x][1]
-
+    for x in item_dictionary.numbers:
+        value = x[0]
+        items = x[1]
         for item in items:
             if item_dictionary.get_adjacent_and_diagonals(item.coordinate[0],item.coordinate[1]):
-                #print(value)
                 sum += int(value)
                 break
 
-        x += 1
-
     print(sum)
-
-
-
 
 if __name__ == "__main__":
     main()
+
